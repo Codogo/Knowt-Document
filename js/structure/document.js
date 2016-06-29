cf433ea38c66f7772a430c6c09798fec566380a2
Freddie Ridell <DrVonTrap@gmail.com>
2016-06-29 16:20:24 +0100

    tests and shit and shit

--- js/structure/document.js
+++ js/structure/document.js
@@ -1,7 +1,12 @@
+var Base64 = require('js-base64').Base64;
+
 class KnowtDocument{
 	constructor(){
-		this.id = btoa(Math.floor(Math.random() * (max - min + 1)) + min);
+		const min = 0, max = 340282366920938463463374607431768211456;
+		this.id = Base64.encode(
+			Math.floor(Math.random() * (max - min + 1)) + min
+		);
 	}
-};
+}
 
 module.exports = KnowtDocument;
--- test/structure/documentSpec.js
+++ test/structure/documentSpec.js
@@ -1,7 +1,12 @@
-class KnowtDocument{
-	constructor(){
-		this.id = btoa(Math.floor(Math.random() * (max - min + 1)) + min);
-	}
-};
+var KnowtDoc = require("../../js/structure/document");
 
-module.exports = KnowtDocument;
+describe("Document Attributes", function(){
+	it("Has Unique ID", function(){
+		var kd1 = new KnowtDoc();
+		var kd2 = new KnowtDoc();
+		expect(kd1.id).not.toBe(null);
+		expect(kd2.id).not.toBe(null);
+		expect(kd1.id).not.toEqual(kd2.id);
+	});
+});
+
